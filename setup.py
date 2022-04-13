5ac141a56261572f88f2407eed00242046532f05
Davide Ria <d.ria@frankhood.it>
2022-04-13 17:25:50 +0200

    Cleaned setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,8 @@
 # -*- coding: utf-8 -*-
 import os
 import re
-import sys
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
+from setuptools import setup
 
 def get_version(*file_paths):
     """Retrieves the version from hooks/__init__.py"""
@@ -23,53 +18,4 @@ def get_version(*file_paths):
 
 version = get_version("hooks/__init__.py")
 
-
-if sys.argv[-1] == 'publish':
-    try:
-        import wheel
-        print("Wheel version: ", wheel.__version__)
-    except ImportError:
-        print('Wheel library missing. Please run "pip install wheel"')
-        sys.exit()
-    os.system('python setup.py sdist upload')
-    os.system('python setup.py bdist_wheel upload')
-    sys.exit()
-
-if sys.argv[-1] == 'tag':
-    print("Tagging the version on git:")
-    os.system("git tag -a %s -m 'version %s'" % (version, version))
-    os.system("git push --tags")
-    sys.exit()
-
-readme = open('README.rst').read()
-history = open('HISTORY.rst').read().replace('.. :changelog:', '')
-requirements = open('requirements.txt').readlines()
-
-setup(
-    name='pre-commit-hooks-django-migrations',
-    version=version,
-    description="""Hook for pre-commit and Django migrations""",
-    long_description=readme + '\n\n' + history,
-    author='FrankHood Business Solutions srl',
-    author_email='info@frankhood.it',
-    url='https://github.com/frankhood/pre-commit-hooks-django-migrations',
-    packages=[
-        'pre_commit_hooks_django_migrations',
-    ],
-    include_package_data=True,
-    install_requires=requirements,
-    license="MIT",
-    zip_safe=False,
-    keywords='pre-commit-hooks-django-migrations',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Framework :: Django :: 3.2',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-    ],
-)
+setup()
