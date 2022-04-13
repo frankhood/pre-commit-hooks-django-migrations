8997e05deab2d7092491821385a3fe23e285b088
Davide Ria <d.ria@frankhood.it>
2022-04-13 17:08:19 +0200

    Fixed pre-commit-hooks.yml

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except ImportError:
 
 
 def get_version(*file_paths):
-    """Retrieves the version from pre_commit_hooks_django_migrations/__init__.py"""
+    """Retrieves the version from hooks/__init__.py"""
     filename = os.path.join(os.path.dirname(__file__), *file_paths)
     version_file = open(filename).read()
     version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
@@ -21,7 +21,7 @@ def get_version(*file_paths):
     raise RuntimeError('Unable to find version string.')
 
 
-version = get_version("pre_commit_hooks_django_migrations/__init__.py")
+version = get_version("hooks/__init__.py")
 
 
 if sys.argv[-1] == 'publish':
