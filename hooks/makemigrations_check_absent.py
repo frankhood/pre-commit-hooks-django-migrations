945676fb5b519ce12540f9f4439c8f88a934c09b
Antonio Mustich <a.mustich@frankhood.it>
2025-05-26 10:44:59 +0200

    Fix print

--- hooks/makemigrations_check_absent.py
+++ hooks/makemigrations_check_absent.py
@@ -12,32 +12,18 @@ MAKEMIGRATIONS_CHECK_CMD = [
 
 
 def get_absent_migrations():
+    output = subprocess.check_output(MAKEMIGRATIONS_CHECK_CMD)
     try:
         output = subprocess.check_output(MAKEMIGRATIONS_CHECK_CMD)
-        sys.stdout.write("####################")
-        sys.stdout.write(f"output: {output}")
-        sys.stdout.write("####################")
     except Exception as ex:
-        sys.stdout.write("####################")
-        sys.stdout.write(f"eccezione {str(ex)}")
-        sys.stdout.write("####################")
         output = ex.output
-        sys.stdout.write("####################")
-        sys.stdout.write(f"eccezione ex.output {output}")
-        sys.stdout.write("####################")
     return output.decode().split("\n")
 
 
 def main() -> int:
     absent_migrations = False
     for filename in get_absent_migrations():
-        sys.stdout.write("####################")
-        sys.stdout.write(f"filename {filename}")
-        sys.stdout.write("####################")
         if re.match(r".*/migrations/.*\.py", filename.strip()):
-            sys.stdout.write("####################")
-            sys.stdout.write(f're.match {re.match(r".*/migrations/.*\.py", filename.strip())}')
-            sys.stdout.write("####################")
             if not (
                 re.match(r".*/root/src/.*\.py", filename)
                 or re.match(r".*/.virtualenvs/.*\.py", filename)
@@ -45,13 +31,8 @@ def main() -> int:
                 or re.match(r".*/site-packages/.*\.py", filename)
             ):
                 absent_migrations = True
-                sys.stdout.write("####################")
-                sys.stdout.write("break")
-                sys.stdout.write("####################")
+                raise Exception()
                 break
-            sys.stdout.write("####################")
-            sys.stdout.write("absent_migrations {absent_migrations}")
-            sys.stdout.write("####################")
     if absent_migrations:
         return 1
     return 0
