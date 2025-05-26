7134cbd11f8464e6382d3709e6e4cb5754ea531c
Antonio Mustich <a.mustich@frankhood.it>
2025-05-26 15:52:03 +0200

    Migration to django package

--- pre_commit_absent_migrations/management/commands/makemigrations_check_absent_command.py
+++ pre_commit_absent_migrations/management/commands/makemigrations_check_absent_command.py
@@ -1,10 +1,26 @@
 
 
+import re
 from django.core.management.base import BaseCommand
+
+from pre_commit_absent_migrations.makemigrations_check_absent import get_absent_migrations
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        from pre_commit_absent_migrations import makemigrations_check_absent
-        makemigrations_check_absent()
+        absent_migrations = False
+        for filename in get_absent_migrations():
+            if re.match(r".*/migrations/.*\.py", filename.strip()):
+                if not (
+                    re.match(r".*/root/src/.*\.py", filename)
+                    or re.match(r".*/.virtualenvs/.*\.py", filename)
+                    or re.match(r".*/venv/.*\.py", filename)
+                    or re.match(r".*/site-packages/.*\.py", filename)
+                ):
+                    absent_migrations = True
+                    break
+        if absent_migrations:
+            return 1
+        return 0
+
         
