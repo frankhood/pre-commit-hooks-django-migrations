108fb7c9339b46ac81703e4c9c5c664669ce078d
Antonio Mustich <a.mustich@frankhood.it>
2025-05-26 12:04:27 +0200

    Fix print

--- hooks/makemigrations_check_absent.py
+++ hooks/makemigrations_check_absent.py
@@ -13,7 +13,10 @@ MAKEMIGRATIONS_CHECK_CMD = [
 
 def get_absent_migrations():
     try:
-        output = subprocess.check_output(MAKEMIGRATIONS_CHECK_CMD)
+        output = subprocess.check_output(
+            MAKEMIGRATIONS_CHECK_CMD,
+            stderr=subprocess.STDOUT
+        )
     except Exception as ex:
         print("#################", vars(ex))
         raise Exception(str(vars(ex)))
