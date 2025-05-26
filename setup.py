48e3a7059e5975f26bf7b2713b3d5cbc76953926
Antonio Mustich <a.mustich@frankhood.it>
2025-05-26 15:15:45 +0200

    Switch todjango package

--- setup.py
+++ setup.py
@@ -2,6 +2,13 @@
 # -*- coding: utf-8 -*-
 import os
 import re
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
 
 from setuptools import setup
 
@@ -18,6 +25,55 @@ def get_version(*file_paths):
 
 version = get_version("hooks/__init__.py")
 
+
+if sys.argv[-1] == 'publish':
+    try:
+        import wheel
+        print("Wheel version: ", wheel.__version__)
+    except ImportError:
+        print('Wheel library missing. Please run "pip install wheel"')
+        sys.exit()
+    os.system('python setup.py sdist upload')
+    os.system('python setup.py bdist_wheel upload')
+    sys.exit()
+
+if sys.argv[-1] == 'tag':
+    print("Tagging the version on git:")
+    os.system("git tag -a %s -m 'version %s'" % (version, version))
+    os.system("git push --tags")
+    sys.exit()
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+requirements = open('requirements.txt').readlines()
+
 setup(
-    version=version
+    name='pre-commit-absent-migrations',
+    version=version,
+    description="""Pre-commit hook for absent migration""",
+    long_description=readme + '\n\n' + history,
+    author='FrankHood Business Solutions srl',
+    author_email='info@frankhood.it',
+    url='https://github.com/frankhood/pre-commit-hooks-django-migrations',
+    packages=[
+        'hooks',
+    ],
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT",
+    zip_safe=False,
+    keywords='hooks',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django :: 3.2',
+        'Framework :: Django :: 4.2',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.11',
+    ],
 )
