424286787de845adb1aeede0edb38eafee1585b6
Antonio Mustich <a.mustich@frankhood.it>
2025-05-26 09:58:31 +0200

    Wip tests

--- hooks/makemigrations_check_absent.py
+++ hooks/makemigrations_check_absent.py
@@ -1,5 +1,6 @@
 import re
 import subprocess
+import sys
 
 MAKEMIGRATIONS_CHECK_CMD = [
     "python",
@@ -13,15 +14,20 @@ MAKEMIGRATIONS_CHECK_CMD = [
 def get_absent_migrations():
     try:
         output = subprocess.check_output(MAKEMIGRATIONS_CHECK_CMD)
+        sys.stderr.write("output", output)
     except Exception as ex:
+        sys.stderr.write("eccezione", str(ex))
         output = ex.output
+        sys.stderr.write("eccezione ex.output", output)
     return output.decode().split("\n")
 
 
 def main() -> int:
     absent_migrations = False
     for filename in get_absent_migrations():
+        sys.stderr.write("filename", filename)
         if re.match(r".*/migrations/.*\.py", filename.strip()):
+            sys.stderr.write("re.match", re.match(r".*/migrations/.*\.py", filename.strip()))
             if not (
                 re.match(r".*/root/src/.*\.py", filename)
                 or re.match(r".*/.virtualenvs/.*\.py", filename)
@@ -29,7 +35,9 @@ def main() -> int:
                 or re.match(r".*/site-packages/.*\.py", filename)
             ):
                 absent_migrations = True
+                sys.stderr.write("break")
                 break
+            sys.stderr.write("absent_migrations", absent_migrations)
     if absent_migrations:
         return 1
     return 0
